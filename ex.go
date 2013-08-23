aa5c363c4ad0335f3f43a01a99c39ea470b87b92
hydra13142 <snake_dragon@126.com>
2013-08-23 23:59:33 +0800

    扩展default使之能支持宏，但omission仍只能支持函数

--- ex.go
+++ ex.go
@@ -41,49 +41,68 @@ func EX() {
 		return b, err
 	})
 	Add("default", func(t []Token, p *Lisp) (Token, error) {
-		var x, y, z Token
-		var err error
+		var (
+			x, y, z Token
+			err     error
+		)
 		x, err = p.Exec(t[0])
 		if err != nil {
 			return None, err
 		}
-		if x.Kind != Front {
-			return None, ErrFitType
-		}
-		f := x.Text.(Lfac)
-		n := f.Para
-		if len(n) < len(t)-1 {
-			return None, ErrParaNum
-		}
-		hold := make([]Token, 0, len(t)-1)
-		for _, z = range t[1:] {
-			y, err = p.Exec(z)
-			if err != nil {
-				return None, err
-			}
-			hold = append(hold, y)
-		}
-		return Token{Back, Gfac(func(t2 []Token, p2 *Lisp) (Token, error) {
-			q := &Lisp{dad: p2, env: map[Name]Token{}}
-			if len(t2) > len(n) || len(t2)+len(hold) < len(n) {
+		switch x.Kind {
+		case Front:
+			f := x.Text.(Lfac)
+			n := f.Para
+			if len(n) < len(t)-1 {
 				return None, ErrParaNum
 			}
-			for m, n := range f.Make {
-				q.env[m] = n
-			}
-			var i, j int
-			for i, z = range t2 {
+			hold := make([]Token, 0, len(t)-1)
+			for _, z = range t[1:] {
 				y, err = p.Exec(z)
 				if err != nil {
 					return None, err
 				}
-				q.env[n[i]] = y
+				hold = append(hold, y)
 			}
-			for i, j = len(n)-1, 1; i >= len(t2); i, j = i-1, j+1 {
-				q.env[n[i]] = hold[len(hold)-j]
+			return Token{Back, Gfac(func(t2 []Token, p2 *Lisp) (Token, error) {
+				q := &Lisp{dad: p2, env: map[Name]Token{}}
+				if len(t2) > len(n) || len(t2)+len(hold) < len(n) {
+					return None, ErrParaNum
+				}
+				for m, n := range f.Make {
+					q.env[m] = n
+				}
+				for i, z := range t2 {
+					y, err = p.Exec(z)
+					if err != nil {
+						return None, err
+					}
+					q.env[n[i]] = y
+				}
+				for i, j := len(n)-1, 1; i >= len(t2); i, j = i-1, j+1 {
+					q.env[n[i]] = hold[len(hold)-j]
+				}
+				return q.Exec(Token{List, f.Text})
+			})}, nil
+		case Macro:
+			g := x.Text.(Macr)
+			n := g.Para
+			if len(n) < len(t)-1 {
+				return None, ErrParaNum
 			}
-			return q.Exec(Token{List, x.Text.(Lfac).Text})
-		})}, nil
+			hold := t[1:]
+			return Token{Back, Gfac(func(t2 []Token, p2 *Lisp) (Token, error) {
+				if len(t2) > len(n) || len(t2)+len(hold) < len(n) {
+					return None, ErrParaNum
+				}
+				m := make([]Token, len(n)+1)
+				m[0] = x
+				copy(m[1:], t2)
+				copy(m[len(t2)+1:], hold[len(t2)+len(hold)-len(n):])
+				return p2.Exec(Token{List, m})
+			})}, nil
+		}
+		return None, ErrFitType
 	})
 	Add("omission", func(t []Token, p *Lisp) (Token, error) {
 		var x, y Token
@@ -95,37 +114,37 @@ func EX() {
 		if err != nil {
 			return None, err
 		}
-		if x.Kind != Front {
-			return None, ErrFitType
+		switch x.Kind {
+		case Front:
+			f := x.Text.(Lfac)
+			n := f.Para
+			return Token{Back, Gfac(func(t2 []Token, p2 *Lisp) (Token, error) {
+				q := &Lisp{dad: p2, env: map[Name]Token{}}
+				if len(t2) < len(n)-1 {
+					return None, ErrParaNum
+				}
+				for m, n := range f.Make {
+					q.env[m] = n
+				}
+				for i := len(n) - 2; i >= 0; i-- {
+					y, err = p.Exec(t2[i])
+					if err != nil {
+						return None, err
+					}
+					q.env[n[i]] = y
+				}
+				z := make([]Token, 0, len(t2)-len(n)+1)
+				for i := len(n) - 1; i < len(t2); i++ {
+					y, err = p.Exec(t2[i])
+					if err != nil {
+						return None, err
+					}
+					z = append(z, y)
+				}
+				q.env[n[len(n)-1]] = Token{List, z}
+				return q.Exec(Token{List, f.Text})
+			})}, nil
 		}
-		f := x.Text.(Lfac)
-		n := f.Para
-		return Token{Back, Gfac(func(t2 []Token, p2 *Lisp) (Token, error) {
-			q := &Lisp{dad: p2, env: map[Name]Token{}}
-			if len(t2) < len(n)-1 {
-				return None, ErrParaNum
-			}
-			for m, n := range f.Make {
-				q.env[m] = n
-			}
-			var i int
-			for i = len(n) - 2; i >= 0; i-- {
-				y, err = p.Exec(t2[i])
-				if err != nil {
-					return None, err
-				}
-				q.env[n[i]] = y
-			}
-			z := make([]Token, 0, len(t2)-len(n)+1)
-			for i = len(n) - 1; i < len(t2); i++ {
-				y, err = p.Exec(t2[i])
-				if err != nil {
-					return None, err
-				}
-				z = append(z, y)
-			}
-			q.env[n[len(n)-1]] = Token{List, z}
-			return q.Exec(Token{List, x.Text.(Lfac).Text})
-		})}, nil
+		return None, ErrFitType
 	})
 }
